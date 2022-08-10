7e7bfc5c2d1e0cf64269e1d327dbbb520765823b
Milan <milan@mdaverde.com>
2022-08-09 20:58:02 -0400

    feat(plugin/clicky): See cookie domain to be same as root site
    Signed-off-by: Milan <milan@mdaverde.com>

--- plugins/clicky.js
+++ plugins/clicky.js
@@ -2,12 +2,17 @@ module.exports = (context, options) => {
   return {
     name: "clicky-plugin",
     injectHtmlTags() {
-      if (
-        process.env.NODE_ENV === "production" &&
-        process.env.CLICKY_ANALYTICS_ID
-      ) {
+      if (process.env.CLICKY_ANALYTICS_ID) {
         return {
           postBodyTags: [
+            {
+              tagName: "script",
+              attributes: {
+                type: "text/javascript",
+              },
+              innerHTML:
+                'var clicky_custom = clicky_custom || {}; clicky_custom.cookie_domain = ".bpfdeploy.io";',
+            },
             {
               tagName: "script",
               attributes: {
